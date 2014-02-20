0a65b15beba408b12677a1ffe78c1fe4ee49b40f
ernado <ernado@ya.ru>
2014-02-20 13:52:18 +0400

    fixed bug in message sending to vk

--- handlers/message.py
+++ handlers/message.py
@@ -49,7 +49,7 @@ class MessageHandler(Handler):
 
         uid = unicode(friends.get_friend_uid(m.jid_to))
         logger.debug('message to user (%s->%s)' % (jid, uid))
-        if not send_message(jid, m.jid_to, uid):
+        if not send_message(jid, m.body, uid):
             return
         answer = get_answer(stanza, m.jid_from, m.jid_to)
 
