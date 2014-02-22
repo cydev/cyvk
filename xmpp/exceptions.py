8bd9e0125d4af3e99dceb7aca80cd97bfb84f27e
ernado <ernado@ya.ru>
2014-02-23 00:07:23 +0400

    pep8

--- xmpp/exceptions.py
+++ xmpp/exceptions.py
@@ -10,73 +10,97 @@ class StreamError(Exception):
     """
 
 
-class BadFormat(StreamError): pass
+class BadFormat(StreamError):
+    pass
 
 
-class BadNamespacePrefix(StreamError): pass
+class BadNamespacePrefix(StreamError):
+    pass
 
 
-class Conflict(StreamError): pass
+class Conflict(StreamError):
+    pass
 
 
-class ConnectionTimeout(StreamError): pass
+class ConnectionTimeout(StreamError):
+    pass
 
 
-class HostGone(StreamError): pass
+class HostGone(StreamError):
+    pass
 
 
-class HostUnknown(StreamError): pass
+class HostUnknown(StreamError):
+    pass
 
 
-class ImproperAddressing(StreamError): pass
+class ImproperAddressing(StreamError):
+    pass
 
 
-class InternalServerError(StreamError): pass
+class InternalServerError(StreamError):
+    pass
 
 
-class InvalidFrom(StreamError): pass
+class InvalidFrom(StreamError):
+    pass
 
 
-class InvalidID(StreamError): pass
+class InvalidID(StreamError):
+    pass
 
 
-class InvalidNamespace(StreamError): pass
+class InvalidNamespace(StreamError):
+    pass
 
 
-class InvalidXML(StreamError): pass
+class InvalidXML(StreamError):
+    pass
 
 
-class NotAuthorized(StreamError): pass
+class NotAuthorized(StreamError):
+    pass
 
 
-class PolicyViolation(StreamError): pass
+class PolicyViolation(StreamError):
+    pass
 
 
-class RemoteConnectionFailed(StreamError): pass
+class RemoteConnectionFailed(StreamError):
+    pass
 
 
-class ResourceConstraint(StreamError): pass
+class ResourceConstraint(StreamError):
+    pass
 
 
-class RestrictedXML(StreamError): pass
+class RestrictedXML(StreamError):
+    pass
 
 
-class SeeOtherHost(StreamError): pass
+class SeeOtherHost(StreamError):
+    pass
 
 
-class SystemShutdown(StreamError): pass
+class SystemShutdown(StreamError):
+    pass
 
 
-class UndefinedCondition(StreamError): pass
+class UndefinedCondition(StreamError):
+    pass
 
 
-class UnsupportedEncoding(StreamError): pass
+class UnsupportedEncoding(StreamError):
+    pass
 
 
-class UnsupportedStanzaType(StreamError): pass
+class UnsupportedStanzaType(StreamError):
+    pass
 
 
-class UnsupportedVersion(StreamError): pass
+class UnsupportedVersion(StreamError):
+    pass
 
 
-class XMLNotWellFormed(StreamError): pass
+class XMLNotWellFormed(StreamError):
+    pass
