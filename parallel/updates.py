0fe469ec615ccbbe6bbc17dbc6cb9b5539854ba6
ernado <ernado@ya.ru>
2014-02-22 12:22:47 +0400

    optimized updates removed whitespace and unused imports removed unnecessary updates

--- parallel/updates.py
+++ parallel/updates.py
@@ -1,41 +1,21 @@
 # coding=utf-8
 from __future__ import unicode_literals
-import time
 from api.vkapi import get_messages, mark_messages_as_read, method
 from friends import get_friend_jid
+import friends
 import messaging.message
 from messaging.parsing import sorting, escape, escape_name
 from parallel import status, realtime, sending
 from parallel.sending import send_typing_status
 from compatibility import HTMLParser
-
-
-#
-# 0,$message_id,0 -- удаление сообщения с указанным local_id
-# 1,$message_id,$flags -- замена флагов сообщения (FLAGS:=$flags)
-# 2,$message_id,$mask[,$user_id] -- установка флагов сообщения (FLAGS|=$mask)
-# 3,$message_id,$mask[,$user_id] -- сброс флагов сообщения (FLAGS&=~$mask)
-# 4,$message_id,$flags,$from_id,$timestamp,$subject,$text,$attachments -- добавление нового сообщения
-# 8,-$user_id,0 -- друг $user_id стал онлайн
-# 9,-$user_id,$flags -- друг $user_id стал оффлайн ($flags равен 0, если пользователь покинул сайт (например, нажал выход) и 1, если оффлайн по таймауту (например, статус away))
-#
-# 51,$chat_id,$self -- один из параметров (состав, тема) беседы $chat_id были изменены. $self - были ли изменения вызываны самим пользователем
-# 61,$user_id,$flags -- пользователь $user_id начал набирать текст в диалоге. событие должно приходить раз в ~5 секунд при постоянном наборе текста. $flags = 1
-# 62,$user_id,$chat_id -- пользователь $user_id начал набирать текст в беседе $chat_id.
-# 70,$user_id,$call_id -- пользователь $user_id совершил звонок имеющий идентификатор $call_id, дополнительную информацию о звонке можно получить используя метод voip.getCallInfo.
-
+import logging
 
 NEW_MESSAGE = 4
 FRIEND_ONLINE = 8
 FRIEND_OFFLINE = 9
 FRIEND_TYPING_CHAT = 61
 FRIEND_TYPING_GROUP = 62
-
-import logging
-
 logger = logging.getLogger("cyvk")
-
-import friends
 
 
 def process_data(jid, data):
@@ -64,9 +44,8 @@ def send_messages(jid):
     if not jid:
         raise ValueError('user api: unable to send messages for blank jid')
 
-    last_message = realtime.get_last_message(jid)
-
-    messages = get_messages(jid, 200, last_message)
+    last_message_id = realtime.get_last_message(jid)
+    messages = get_messages(jid, 200, last_message_id)
 
     if not messages:
         return
@@ -77,16 +56,14 @@ def send_messages(jid):
         return
 
     read = []
-
-    last_message = messages[-1]["mid"]
-
-    realtime.set_last_message(jid, last_message)
+    last_message_id = messages[-1]["mid"]
+    realtime.set_last_message(jid, last_message_id)
 
     for message in messages:
         read.append(str(message["mid"]))
         from_jid = get_friend_jid(message["uid"])
-        webtools = HTMLParser()
-        body = webtools.unescape(message["body"]).replace('<br>', '\n')
+        parser = HTMLParser()
+        body = parser.unescape(message["body"]).replace('<br>', '\n')
         body += messaging.message.parse(jid, message)
         sending.send(jid, escape("", body), from_jid, message["date"])
 
