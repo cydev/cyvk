f8833b84e3e2ffec7161407840cfb8cfd3e0e2ca
ernado <ernado@ya.ru>
2014-02-28 21:47:35 +0400

    returned back type check

--- transport/stanza_queue.py
+++ transport/stanza_queue.py
@@ -2,11 +2,9 @@ import pickle
 
 import redis
 
-
-# from xmpp import Stanza as Stanza
 from config import REDIS_HOST, REDIS_PORT, REDIS_PREFIX
+from cystanza.stanza import Stanza
 import compat
-# from cystanza.stanza import Stanza as CyStanza
 
 logger = compat.get_logger()
 r = redis.StrictRedis(REDIS_HOST, REDIS_PORT, )
@@ -19,8 +17,6 @@ def _get_stanza_queue_key():
 def enqueue():
     pickled_stanza = r.brpop(_get_stanza_queue_key())[1]
     stanza = pickle.loads(pickled_stanza)
-
-    # if not isinstance(stanza, Stanza) and not isinstance(stanza, CyStanza):
-    #     raise ValueError('expected stanza, deserialized %s' % type(stanza))
-
+    if not isinstance(stanza, Stanza):
+        raise ValueError('expected stanza, deserialized %s' % type(stanza))
     return stanza
